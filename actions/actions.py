dcd044cffb0d398e833247f40330ffaecd144cd1
SSCharan-99 <saisricharan99@msitprogram.net>
2021-06-10 08:10:07 +0530

    charan

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,64 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
+from typing import Any, Text, Dict, List
 
-#     def name(self) -> Text:
-#         return "action_hello_world"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-#         dispatcher.utter_message(text="Hello World!")
 
-#         return []
+class Actionpackage(Action):
+
+    def name(self) -> Text:
+        return "action_package"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities=tracker.latest_message['entities']
+        print(entities)
+        
+        for e in entities:
+            if e['entity'] == 'year':
+                name = e['value']
+            if name == "2020":
+                message="highest package is 24L <a href='https://www.msitprogram.net/'>For more info click here</a>"
+            if name == "2019":
+                message="highest package is 22L"
+            if name == "2018":
+                message="highest package is 20L"
+        #dispatcher.utter_message(text="Hello World!")
+        
+        dispatcher.utter_message(text=message)
+
+        return []
+
+class Actionfee(Action):
+
+    def name(self) -> Text:
+        return "action_fee"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities=tracker.latest_message['entities']
+        print(entities)
+        msg="Fee at JNTUH is 170000, Fee at IITH is 200000, Fee at SVU is 130000 "
+        for e in entities:
+            
+            if e['entity'] == 'institute':
+                name = e['value']
+            if name == "JNTUH":
+                msg="Fee at JNTUH is 170000"
+            if name == "IIIT":
+                msg="Fee at IITH is 200000"
+            if name == "JNTUK":
+                msg="Fee at JNTUK is 130000"
+            if name == "SVU":
+                msg="Fee at SVU is 130000"
+        #dispatcher.utter_message(text="Hello World!")
+        
+        dispatcher.utter_message(text=msg)
+
+        return []
